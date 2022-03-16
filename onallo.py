0e3dc65a04109aa3dc59af38e7908de0c46b3d92
DervadericsMark <78954806+DervadericsMark@users.noreply.github.com>
2022-03-16 11:21:56 +0100

    dik

---
+++ onallo.py
@@ -0,0 +1,15 @@
+from tkinter import *
+ablak=Tk()
+
+elsomezo=Label(ablak,text="Első mező")
+elsomezo.grid(row=0,cloumn=0)
+
+elsoinput=Entry(ablak)
+elsoinput.grid(row=0,cloumn=1)
+
+masodikmezo=Label(ablak,text="Második mező")
+masodikmezi.grid(row=1,cloumn=1)
+
+
+
+ablak.mainloop()
